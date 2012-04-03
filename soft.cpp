fefc0df41551bdba8f101968816b042f59ef5a9a
Tribhuvanesh Orekondy <tribhuvanesh@gmail.com>
2012-04-03 20:45:21 +0530

    Soft biometric traits completed. Working.

--- soft.cpp
+++ soft.cpp
@@ -1,4 +1,4 @@
-// If trying to debug the color detector code, enable SHOW_DEBUG_IMAGE:
+// If trying to debug the colour detector code, enable SHOW_DEBUG_IMAGE:
 #define SHOW_DEBUG_IMAGE 0
 
 #include <cstdio>	// Used for "printf"
@@ -7,9 +7,10 @@
 //#include <cmath>	// Used to calculate square-root for statistics
 
 //ImageUtil includes
-//#include <tchar.h>
 #include <vector>	// Used for C++ vectors
 #include <map>
+#include<algorithm>
+#include<numeric>
 //#include <sstream>	// for printing floats in C++
 //#include <fstream>	// for opening files in C++
 
@@ -22,53 +23,66 @@
 #include "utils.h"
 
 #define AVG_TEMP_COUNT 5
+#define EPSILON 0.01
 bool run_shirt=true;
+
+// Various colour types for detected shirt colours.
+enum                             {cBLACK=0,cWHITE, cGREY, cRED, cORANGE, cYELLOW, cGREEN, cAQUA, cBLUE, cPURPLE, cPINK,  NUM_COLOUR_TYPES};
+char sCTypes[][NUM_COLOUR_TYPES] = {"Black", "White","Grey","Red","Orange","Yellow","Green","Aqua","Blue","Purple","Pink"};
+uchar cCTHue[NUM_COLOUR_TYPES] =    {0,       0,      0,     0,     20,      30,      55,    85,   115,    138,     161};
+uchar cCTSat[NUM_COLOUR_TYPES] =    {0,       0,      0,    255,   255,     255,     255,   255,   255,    255,     255};
+uchar cCTVal[NUM_COLOUR_TYPES] =    {0,      255,    120,   255,   255,     255,     255,   255,   255,    255,     255};
+
+string colour_types[] = {"Black", "White","Grey","Red","Orange","Yellow","Green","Aqua","Blue","Purple","Pink"};
+
+// Function prototypes
+int		    getPixelColorType(int H, int S, int V);
+map<string, float>  get_temp( IplImage*, CvHaarClassifierCascade* );
+map<string, float>  createAverage( vector< map<string, float> > );
+float		    sigmoid( float );
+float		    nrmsd( map<string, float>, map<string, float> );
+
+// Initialize vector with colours
+vector<string> colour_vect(sCTypes, sCTypes + NUM_COLOUR_TYPES);
+
+// Range for normalizing RMSD
+float range_norm = sqrt((2*100*100) / NUM_COLOUR_TYPES);
 
 // Face Detection HaarCascade Classifier file for OpenCV (downloadable from "http://alereimondo.no-ip.org/OpenCV/34").
 const char* cascadeFileFace = "haar/haarcascade_frontalface_alt.xml";	// Path to the Face Detection HaarCascade XML file
 
-// Various color types for detected shirt colors.
-enum                             {cBLACK=0,cWHITE, cGREY, cRED, cORANGE, cYELLOW, cGREEN, cAQUA, cBLUE, cPURPLE, cPINK,  NUM_COLOR_TYPES};
-char sCTypes[][NUM_COLOR_TYPES] = {"Black", "White","Grey","Red","Orange","Yellow","Green","Aqua","Blue","Purple","Pink"};
 
-
-uchar cCTHue[NUM_COLOR_TYPES] =    {0,       0,      0,     0,     20,      30,      55,    85,   115,    138,     161};
-uchar cCTSat[NUM_COLOR_TYPES] =    {0,       0,      0,    255,   255,     255,     255,   255,   255,    255,     255};
-uchar cCTVal[NUM_COLOR_TYPES] =    {0,      255,    120,   255,   255,     255,     255,   255,   255,    255,     255};
-
-string colour_types[] = {"Black", "White","Grey","Red","Orange","Yellow","Green","Aqua","Blue","Purple","Pink"};
-
-// Determine what type of color the HSV pixel is. Returns the colorType between 0 and NUM_COLOR_TYPES.
+// Determine what type of colour the HSV pixel is. Returns the colourType between 0 and NUM_COLOUR_TYPES.
 int getPixelColorType(int H, int S, int V)
 {
-	int color;
+	int colour;
 	if (V < 75)
-		color = cBLACK;
+		colour = cBLACK;
 	else if (V > 190 && S < 27)
-		color = cWHITE;
+		colour = cWHITE;
 	else if (S < 53 && V < 185)
-		color = cGREY;
-	else {	// Is a color
+		colour = cGREY;
+	else {	// Is a colour
 		if (H < 14)
-			color = cRED;
+			colour = cRED;
 		else if (H < 25)
-			color = cORANGE;
+			colour = cORANGE;
 		else if (H < 34)
-			color = cYELLOW;
+			colour = cYELLOW;
 		else if (H < 73)
-			color = cGREEN;
+			colour = cGREEN;
 		else if (H < 102)
-			color = cAQUA;
+			colour = cAQUA;
 		else if (H < 127)
-			color = cBLUE;
+			colour = cBLUE;
 		else if (H < 149)
-			color = cPURPLE;
+			colour = cPURPLE;
 		else if (H < 175)
-			color = cPINK;
+			colour = cPINK;
 		else	// full circle 
-			color = cRED;	// back to Red
+			colour = cRED;	// back to Red
 	}
-	return color;
+	return colour;
 }
 
 
@@ -77,13 +91,13 @@ map<string, float> get_temp(IplImage* imageIn, CvHaarClassifierCascade *cascadeF
 						
 		assert( imageIn != NULL );			
 		//std::cout<< "captured frame"<<std::endl;		
-		//std::cout << "(got a " << imageIn->width << "x" << imageIn->height << " color image)." << std::endl;
+		//std::cout << "(got a " << imageIn->width << "x" << imageIn->height << " colour image)." << std::endl;
 		IplImage* imageDisplay = cvCloneImage(imageIn);
 		map<string, float> cmap;
 
-		// If trying to debug the color detector code, enable this:
+		// If trying to debug the colour detector code, enable this:
 		#ifdef SHOW_DEBUG_IMAGE
-			// Create a HSV image showing the color types of the whole image, for debugging.
+			// Create a HSV image showing the colour types of the whole image, for debugging.
 			IplImage *imageInHSV = cvCreateImage(cvGetSize(imageIn), 8, 3);
 			cvCvtColor(imageIn, imageInHSV, CV_BGR2HSV);	// (note that OpenCV stores RGB images in B,G,R order.
 				IplImage* imageDisplayHSV = cvCreateImage(cvGetSize(imageIn), 8, 3);	// Create an empty HSV image
@@ -99,10 +113,10 @@ map<string, float> get_temp(IplImage* imageIn, CvHaarClassifierCascade *cascadeF
 						uchar H = *(uchar*)(imOfsIn + y*rowSizeIn + x*3 + 0);	// Hue
 						uchar S = *(uchar*)(imOfsIn + y*rowSizeIn + x*3 + 1);	// Saturation
 						uchar V = *(uchar*)(imOfsIn + y*rowSizeIn + x*3 + 2);	// Value (Brightness)
-						// Determine what type of color the HSV pixel is.
+						// Determine what type of colour the HSV pixel is.
 						int ctype = getPixelColorType(H, S, V);
 						//ctype = x / 60;
-						// Show the color type on the displayed image, for debugging.
+						// Show the colour type on the displayed image, for debugging.
 						*(uchar*)(imOfsDisp + (y)*rowSizeIn + (x)*3 + 0) = cCTHue[ctype];	// Hue
 						*(uchar*)(imOfsDisp + (y)*rowSizeIn + (x)*3 + 1) = cCTSat[ctype];	// Full Saturation (except for black & white)
 						*(uchar*)(imOfsDisp + (y)*rowSizeIn + (x)*3 + 2) = cCTVal[ctype];	// Full Brightness
@@ -120,10 +134,12 @@ map<string, float> get_temp(IplImage* imageIn, CvHaarClassifierCascade *cascadeF
 		rectFace = detectFace(imageIn, cascadeFace);
 		//double tallyFaceDetectTime = (double)cvGetTickCount() - timeFaceDetectStart;
 		//cout << "Found " << rectFaces.size() << " faces in " << tallyFaceDetectTime/((double)cvGetTickFrequency()*1000.) << "ms\n";	
+#if DEBUG		
 		cout<< "detected face";
 		// Process each detected face
-		cout << "Detecting shirt color below the face." << endl;
-							
+		cout << "Detecting shirt colour below the face." << endl;
+#endif
+
 		float initialConfidence = 1.0f;
 		int bottom;
 		drawBox(imageDisplay, rectFace, CV_RGB(255,0,0));
@@ -136,7 +152,9 @@ map<string, float> get_temp(IplImage* imageIn, CvHaarClassifierCascade *cascadeF
 		rectShirt.y = rectFace.y + (int)(SHIRT_DY * (float)rectFace.height) + (int)(0.5f * (1.0f-SHIRT_SCALE_Y)* (float)rectFace.height);
 		rectShirt.width = (int)(SHIRT_SCALE_X * rectFace.width);
 		rectShirt.height = (int)(SHIRT_SCALE_Y * rectFace.height);
+#if DEBUG		
 		cout << "Shirt region is from " << rectShirt.x << ", " << rectShirt.y << " to " << rectShirt.x + rectShirt.width - 1 << ", " << rectShirt.y + rectShirt.height - 1 << endl;
+#endif
 		
 		// If the shirt region goes partly below the image, try just a little below the face
 		bottom = rectShirt.y+rectShirt.height-1;
@@ -148,8 +166,10 @@ map<string, float> get_temp(IplImage* imageIn, CvHaarClassifierCascade *cascadeF
 			rectShirt.y = rectFace.y + (int)(SHIRT_DY * (float)rectFace.height) + (int)(0.5f * (1.0f-SHIRT_SCALE_Y) * (float)rectFace.height);
 			rectShirt.height = (int)(SHIRT_SCALE_Y * rectFace.height);
 			initialConfidence = initialConfidence * 0.5f;	// Since we are using a smaller region, we are less confident about the results now.
+#if DEBUG
 			cout << "Warning: Shirt region goes past the end of the image. Trying to reduce the shirt region position to " ;
 			cout<< rectShirt.y << " with a height of " << rectShirt.height << endl;
+#endif		
 		}
 
 		// Try once again if it is partly below the image.
@@ -158,8 +178,10 @@ map<string, float> get_temp(IplImage* imageIn, CvHaarClassifierCascade *cascadeF
 		bottom = imageIn->height-1;	// Limit the bottom
 		rectShirt.height = bottom - (rectShirt.y-1);	// Adjust the height to use the new bottom
 		initialConfidence = initialConfidence * 0.7f;	// Since we are using a smaller region, we are less confident about the results now.
+#if DEBUG
 		cout << "Warning: Shirt region still goes past the end of the image. Trying to reduce the shirt region height to ";
 		cout << rectShirt.height << endl;
+#endif	
 	}
 	// Make sure the shirt region is in the image
 		if (rectShirt.height <= 1) 
@@ -171,7 +193,7 @@ map<string, float> get_temp(IplImage* imageIn, CvHaarClassifierCascade *cascadeF
 	
 			// Show the shirt region
 			drawBox(imageDisplay, rectShirt, CV_RGB(255,255,255));
-			// Convert the shirt region from RGB colors to HSV colors
+			// Convert the shirt region from RGB colours to HSV colours
 			//cout << "Converting shirt region to HSV" << endl;
 			IplImage *imageShirt = cropImage(imageIn, rectShirt);
 			IplImage *imageShirtHSV = cvCreateImage(cvGetSize(imageShirt), 8, 3);
@@ -180,16 +202,16 @@ map<string, float> get_temp(IplImage* imageIn, CvHaarClassifierCascade *cascadeF
 				cerr << "ERROR: Couldn't convert Shirt image from BGR2HSV." << endl;
 				exit(1);
 			}
-			//cout << "Determining color type of the shirt" << endl;
+			//cout << "Determining colour type of the shirt" << endl;
 			int h = imageShirtHSV->height;				// Pixel height
 			int w = imageShirtHSV->width;				// Pixel width
 			int rowSize = imageShirtHSV->widthStep;		// Size of row in bytes, including extra padding
 			char *imOfs = imageShirtHSV->imageData;	// Pointer to the start of the image HSV pixels.
-				// Create an empty tally of pixel counts for each color type
-				int tallyColors[NUM_COLOR_TYPES];
-				for (int i=0; i<NUM_COLOR_TYPES; i++)
+				// Create an empty tally of pixel counts for each colour type
+				int tallyColors[NUM_COLOUR_TYPES];
+				for (int i=0; i<NUM_COLOUR_TYPES; i++)
 					tallyColors[i] = 0;
-				// Scan the shirt image to find the tally of pixel colors
+				// Scan the shirt image to find the tally of pixel colours
 				for (int y=0; y<h; y++) 
 				{
 					for (int x=0; x<w; x++) {
@@ -198,21 +220,23 @@ map<string, float> get_temp(IplImage* imageIn, CvHaarClassifierCascade *cascadeF
 						uchar S = *(uchar*)(imOfs + y*rowSize + x*3 + 1);	// Saturation
 						uchar V = *(uchar*)(imOfs + y*rowSize + x*3 + 2);	// Value (Brightness)
 	
-						// Determine what type of color the HSV pixel is.
+						// Determine what type of colour the HSV pixel is.
 						int ctype = getPixelColorType(H, S, V);
-						// Keep count of these colors.
+						// Keep count of these colours.
 						tallyColors[ctype]++;
 					}
 			}
-			// Print a report about color types, and find the max tally
-			//cout << "Number of pixels found using each color type (out of " << (w*h) << ":\n";
+			// Print a report about colour types, and find the max tally
+			//cout << "Number of pixels found using each colour type (out of " << (w*h) << ":\n";
 			int tallyMaxIndex = 0;
 			int tallyMaxCount = -1;
 			int pixels = w * h;
-			for (int i=0; i<NUM_COLOR_TYPES; i++) 
+			for (int i=0; i<NUM_COLOUR_TYPES; i++) 
 			{
 				int v = tallyColors[i];
+#if DEBUG				
 				cout << sCTypes[i] << " " << (v*100/pixels) << "%, ";
+#endif				
 				string col = sCTypes[i];
 				cmap[col] = (v * 100 / pixels);
 				if (v > tallyMaxCount) 
@@ -223,9 +247,10 @@ map<string, float> get_temp(IplImage* imageIn, CvHaarClassifierCascade *cascadeF
 			}
 			cout << endl;
 			int percentage = initialConfidence * (tallyMaxCount * 100 / pixels);
+#if DEBUG
 			cout << "Color of shirt: " << sCTypes[tallyMaxIndex] << " (" << percentage << "% confidence)." << endl << endl;
-
-			// Display the color type over the shirt in the image.
+#endif
+			// Display the colour type over the shirt in the image.
 			CvFont font;
 			//cvInitFont(&font,CV_FONT_HERSHEY_PLAIN,0.55,0.7, 0,1,CV_AA);	// For OpenCV 1.1
 			cvInitFont(&font,CV_FONT_HERSHEY_PLAIN,0.8,1.0, 0,1, CV_AA);	// For OpenCV 2.0
@@ -257,21 +282,48 @@ map<string, float> createAverage( vector< map<string, float> > cmap_vect )
 	int i, j;
 	map<string, float> avgTemplate;
 
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < colour_vect.size(); i++)
 	{
-		string col = colour_types[i];
 		for (j = 0; j < cmap_vect.size(); j++)
 		{
-			avgTemplate[col] += cmap_vect[j][col];
+			avgTemplate[colour_vect[i]] += cmap_vect[j][colour_vect[i]];
 		}
-		avgTemplate[col] /= cmap_vect.size();
+		avgTemplate[colour_vect[i]] /= cmap_vect.size();
 	}
 
 	return avgTemplate;
 }
 
-int main()
+float sigmoid(float x)
 {
+	float exp_val = exp(-x);
+	return 1 / (1 + exp_val);
+}
+
+float nrmsd(map<string, float> avgTemplate, map<string, float> currentTemplate)
+{
+	int i;
+	float rmsd;
+	vector<float> delta (NUM_COLOUR_TYPES, 0);
+	//for_each(colour_vect.begin(), colour_vect.end(), func)
+
+	for (i = 0; i < colour_vect.size(); i++) 
+	{
+		delta[i] = avgTemplate[colour_vect[i]] - currentTemplate[colour_vect[i]];
+		delta[i] *= delta[i];
+	}
+
+	rmsd = sqrt( accumulate(delta.begin(), delta.end(), 0) / delta.size() );
+
+	//cout<<"RMSD = "<<rmsd;
+	//cout<<"\t Range = "<<range_norm<<endl;
+
+	return 1 - rmsd/range_norm;
+}
+
+int main(int argc, char** argv)
+{
+	int i;
 	IplImage * imageIn;
 	CvCapture* capture;
 	cvNamedWindow("Shirt", CV_WINDOW_AUTOSIZE);
@@ -279,6 +331,9 @@ int main()
 	printf("Initialized template");
 	//cvShowImage("Shirt", init_templ->shirt_hsv);
 	
+	map<string, float>::iterator it;
+	map<string, float> avgTemplate;
+
 	capture = cvCreateCameraCapture(getenv("CAM")==NULL? -1:1);
 	assert( capture != NULL );
 
@@ -293,7 +348,7 @@ int main()
 
 	vector< map<string, float> > cmap_vect;
 
-	int avg_count = 5;
+	// Creating average template by using the next 5 valid frames
 	while(true)
 	{
 		imageIn = cvQueryFrame(capture);
@@ -320,9 +375,10 @@ int main()
 		
 	}
 
+	avgTemplate = createAverage(cmap_vect);
+
+#if DEBUG
 	cout<<"Printing the 5 colours templates"<<endl;
-	int i;
-	map<string, float>::iterator it;
 	for (i = 0; i < cmap_vect.size(); i++)
 	{
 		for (it = cmap_vect[i].begin(); it != cmap_vect[i].end(); it++)
@@ -332,14 +388,41 @@ int main()
 		cout<<endl;	
 	}
 
-	map<string, float> avgTemplate = createAverage(cmap_vect);
 	cout<<"Average template: "<<endl;
 	for (it = avgTemplate.begin(); it != avgTemplate.end(); it++)
 	{
 		cout<<(*it).first<<" => "<<(*it).second<<"  ";
 	}
+#endif
 
+	string states[] = {"Lock-down", "Authorized"};
+	bool current_state = 1;
+	while(true)
+	{
+		imageIn = cvQueryFrame(capture);
+		map<string, float> currentmap = get_temp(imageIn, cascadeFace);
 
+		if(currentmap.size() != 0)
+		{	
+			float confidence = nrmsd(avgTemplate, currentmap);
+			bool new_state = ( (confidence >= 0.5)? 1 : 0 );
+#if DEBUG		
+			cout<<"confidence: "<<confidence;
+#endif			
+			//cout<<(( sigmoid(confidence) <= EPSILON )?"Authorized":"Lock-down")<<endl;
+			//cout<<( (confidence >= 0.5)?"Authorized":"Lock-down" )<<endl;
+			
+			if(current_state != new_state)
+				current_state = new_state;
+			cout<<states[(int)current_state];
+		}
+		else
+			continue;
+
+		char c = cvWaitKey(30);
+		if( c == 27 )
+			break;
+	}
 	cvDestroyWindow("Shirt");
 
 	cvReleaseImage(&imageIn);
